35440ab239589f26b1897ae961f2096039c8ec95
cbsuleman <suleman@cruisebrains.com>
2025-04-23 10:26:29 +0500

    Optimize images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -155,6 +155,7 @@ function Team() {
                           <Image
                             alt=""
                             {...person.image}
+                            quality={20}
                             className="h-96 w-full object-cover grayscale transition duration-500 motion-safe:group-hover:scale-105"
                           />
                           <div className="absolute inset-0 flex flex-col justify-end bg-linear-to-t from-black to-black/0 to-40% p-6">
